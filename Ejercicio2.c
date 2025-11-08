d3acf28853384e9ee29440f0b9f03f10fc025ec0
Uli__ <ulisescolin332@gmail.com>
2025-11-07 22:55:24 -0600

    Create Ejercicio2.c

---
+++ Ejercicio2.c
@@ -0,0 +1,22 @@
+/*Francisco Colín Ulises
+Tarea 2, Ejercicio 2*/
+
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    
+    int r = 0;
+    float Area = 0, Circunferencia = 0;
+    
+    printf("Ingrese el radio del circulo: ");
+    scanf("%d", &r);
+    
+    Area = (3.141592 * pow(r,2));
+    Circunferencia = 2 * 3.141592 * r;
+    
+    printf("\nArea del circulo = %f\n", Area);
+    printf("Perimetro de la circunferencia = %f", Circunferencia);
+    
+}
