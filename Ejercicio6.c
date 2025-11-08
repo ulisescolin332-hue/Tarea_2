514293c536378b2bcd3b9e431fac0a9ba961373d
Uli__ <ulisescolin332@gmail.com>
2025-11-08 00:05:45 -0600

    Create Ejercicio6.c

---
+++ Ejercicio6.c
@@ -0,0 +1,34 @@
+/*Francisco Colín Ulises
+Tarea 2, Ejercicio 6*/
+
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    
+    float sueldo = 0;
+    float sueldototal = 0;
+    
+    printf("Ingrese el sueldo percibido: ");
+    scanf("%f", &sueldo);
+    
+    if (sueldo > 0)
+    {
+        if (sueldo < 1000)
+        {
+            sueldototal = (sueldo + (sueldo * 0.15));
+        }
+        else
+        {
+            sueldototal = (sueldo + (sueldo * 0.12));
+        }
+        
+        printf("\nEl sueldo con el aumento incorporado es = %f", sueldototal);  
+    }
+    else
+    {
+        printf("Sueldo percibido ingresado no valido");
+    }
+    
+}
