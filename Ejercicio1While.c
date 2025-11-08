121a5d7e1e133c0fdca2bdf702fb0c7fc1355c27
Uli__ <ulisescolin332@gmail.com>
2025-11-08 15:23:11 -0600

    Create Ejercicio1While.c

---
+++ Ejercicio1While.c
@@ -0,0 +1,25 @@
+/*Francisco Colín Ulises
+Tarea 2, Ejercicio 1*/
+
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    
+    int n = 0, hasta = 0;
+    int num = 1, resul = 0;
+    
+    printf("Ingrese el numero del cual requiere su tabla de multiplicar: ");
+    scanf("%d", &n);
+    printf("Ingrese hasta donde requiere su extension: ");
+    scanf("%d", &hasta);
+    
+    while (num <= hasta)
+    {
+        resul = n * num;
+        printf("\n%d * %d = %d", n, num, resul); 
+        
+        num = num + 1;
+    }
+}
