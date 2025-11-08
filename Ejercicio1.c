24be49279a28b0565264461565980ec7570fba7f
Uli__ <ulisescolin332@gmail.com>
2025-11-07 23:04:09 -0600

    Update Ejercicio1.c

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -7,20 +7,20 @@ Tarea 2, Ejercicio 1*/
 void main()
 {
     
-    int a = 0, b = 0;
-    int sum = 0, res = 0, mul = 0;
+    float a = 0, b = 0;
+    float sum = 0, res = 0, mul = 0;
     
     printf("Ingrese un numero: ");
-    scanf("%d", &a);
+    scanf("%f", &a);
     printf("Ingrese otro numero: ");
-    scanf("%d", &b);
+    scanf("%f", &b);
     
     sum = a + b;
     res = a - b;
     mul = a * b;
     
-    printf("\n%d + %d = %d\n", a, b, sum);
-    printf("%d - %d = %d\n", a, b, res);
-    printf("%d * %d = %d\n", a, b, mul);
+    printf("\n%f + %f = %f\n", a, b, sum);
+    printf("%f - %f = %f\n", a, b, res);
+    printf("%f * %f = %f\n", a, b, mul);
 
 }
