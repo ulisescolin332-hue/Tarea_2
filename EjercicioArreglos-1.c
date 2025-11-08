f0cea46ce3b94b115ecf562b93991e668d5b7dbc
Uli__ <ulisescolin332@gmail.com>
2025-11-08 17:32:19 -0600

    Create EjercicioArreglos-1.c

---
+++ EjercicioArreglos-1.c
@@ -0,0 +1,32 @@
+/*Francisco Colín Ulises
+Tarea 2, Ejercicio 1*/
+
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    int arr[10];
+    int elementos = 0;
+    int i = 0, sum = 0;
+    
+    do
+    {
+        printf("Proporciona el numero de elementos del arreglo (1 al 10): ");
+        scanf("%d", &elementos);
+    }
+    while ((elementos < 1) || (elementos > 10));
+    
+    for (i = 0; i < elementos; i = i + 1)
+    {
+        printf("ingrese un valor entero: ");
+        scanf("%d", &arr[i]);
+    }
+    
+    for (i = 0; i < elementos; i = i + 1)
+    {
+        sum = sum + arr[i];
+    }
+    
+    printf("\nLa suma de los elementos ingresados es = %d", sum);
+}
