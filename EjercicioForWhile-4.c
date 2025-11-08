62ae80db75ce9ea84c42ce6481aa73afbd9104f8
Uli__ <ulisescolin332@gmail.com>
2025-11-08 16:41:53 -0600

    Create EjercicioForWhile-4.c

---
+++ EjercicioForWhile-4.c
@@ -0,0 +1,30 @@
+/*Francisco Colín Ulises
+Tarea 2, Ejercicio 4*/
+
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    
+    int n = 0;
+    int num = 1, mult = 1;
+    
+    printf("Indique hasta que numero quiere que se haga la multiplicacion de numeros naturales: ");
+    
+    do
+    {
+        scanf("%d", &n); 
+    }
+    while (n < 1);
+    
+
+    while (num <= n)
+    {
+        mult = mult * num;
+        num = num + 1;
+    }
+    
+    printf("\nLa multiplicacion de los primeros  %d  numeros naturales es =  %d", n, mult);
+
+}
