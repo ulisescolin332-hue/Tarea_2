b14491c6df7669ecd1b12b73fd6ff6b817758953
Uli__ <ulisescolin332@gmail.com>
2025-11-14 20:33:44 -0600

    Update EjercicioArreglos-1.c

--- EjercicioArreglos-1.c
+++ EjercicioArreglos-1.c
@@ -7,27 +7,20 @@ Tarea 2, Ejercicio 1*/
 void main()
 {
     int arr[10];
-    int elementos = 0;
     int i = 0, sum = 0;
-    
-    do
+
+    for (i; i < 10; i = i + 1)
     {
-        printf("Proporciona el numero de elementos del arreglo (1 al 10): ");
-        scanf("%d", &elementos);
-    }
-    while ((elementos < 1) || (elementos > 10));
-    
-    for (i; i < elementos; i = i + 1)
-    {
-        printf("ingrese un valor entero: ");
+        printf("Ingrese un valor entero: ");
         scanf("%d", &arr[i]);
     }
 
     i = 0;
-    for (i; i < elementos; i = i + 1)
+    
+    for (i; i < 10; i = i + 1)
     {
         sum = sum + arr[i];
     }
-    
+
     printf("\nLa suma de los elementos ingresados es = %d", sum);
 }
